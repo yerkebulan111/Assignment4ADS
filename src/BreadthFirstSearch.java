4947d01db11e540e55c86e030e238057463cb3d1
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-05-23 02:44:11 +0500

    Commit 5

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,25 @@
-public class BreadthFirstSearch {
+import java.util.*;
+
+public class BreadthFirstSearch<T> extends Search<T> {
+    public BreadthFirstSearch(UnweightedGraph<T> graph, T startValue) {
+        super(new Vertex<>(startValue));
+        bfs(graph);
+    }
+
+    private void bfs(UnweightedGraph<T> graph) {
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        queue.add(start);
+        marked.put(start, true);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.poll();
+            for (Vertex<T> neighbor : graph.getAdjVertices(current)) {
+                if (!marked.containsKey(neighbor)) {
+                    edgeTo.put(neighbor, current);
+                    marked.put(neighbor, true);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 }
