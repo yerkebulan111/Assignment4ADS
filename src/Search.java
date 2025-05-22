c418b91a585643854cbddfe03dc94ae56d02e1a6
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-05-23 02:06:39 +0500

    Commit 3

--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,22 @@
-public class Search {
+import java.util.*;
+
+public abstract class Search<T> {
+    protected final Map<Vertex<T>, Boolean> marked = new HashMap<>();
+    protected final Map<Vertex<T>, Vertex<T>> edgeTo = new HashMap<>();
+    protected final Vertex<T> start;
+
+    public Search(Vertex<T> start) {
+        this.start = start;
+    }
+
+    public List<T> pathTo(T end) {
+        Vertex<T> endVertex = new Vertex<>(end);
+        if (!marked.containsKey(endVertex)) return Collections.emptyList();
+
+        LinkedList<T> path = new LinkedList<>();
+        for (Vertex<T> v = endVertex; v != null; v = edgeTo.get(v)) {
+            path.addFirst(v.getValue());
+        }
+        return path;
+    }
 }
