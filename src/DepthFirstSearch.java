d0dde54017e79d9a498b940078d90c1bb8e11339
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-05-23 02:24:15 +0500

    Commit 4

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -1,2 +1,16 @@
-public class DepthFirstSearch {
+public class DepthFirstSearch<T> extends Search<T> {
+    public DepthFirstSearch(UnweightedGraph<T> graph, T startValue) {
+        super(new Vertex<>(startValue));
+        dfs(graph, this.start);
+    }
+
+    private void dfs(UnweightedGraph<T> graph, Vertex<T> current) {
+        marked.put(current, true);
+        for (Vertex<T> neighbor : graph.getAdjVertices(current)) {
+            if (!marked.containsKey(neighbor)) {
+                edgeTo.put(neighbor, current);
+                dfs(graph, neighbor);
+            }
+        }
+    }
 }
