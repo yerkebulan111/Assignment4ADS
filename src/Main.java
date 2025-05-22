81e1186823eb8be0dba5fcead0701f71a3401ab0
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-05-23 03:41:42 +0500

    Commit 8

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,28 @@ public class Main {
         UnweightedGraph<String> graph = new UnweightedGraph<>(true);
         fillWithoutWeights(graph);
 
+        System.out.println();
         System.out.println("DFS:");
         Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
         outputPath(dfs, "Kyzylorda");
+
+
+
+        System.out.println("----------------------");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
+
+        System.out.println("----------------------");
+
+        WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
+        fillWithWeights(weightedGraph);
+
+        System.out.println("Dijkstra:");
+        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
+        outputPath(djk, "Kyzylorda");
+
     }
 
     public static void fillWithoutWeights(UnweightedGraph<String> graph) {
@@ -18,10 +37,25 @@ public class Main {
         graph.addEdge("Shymkent", "Kyzylorda");
     }
 
+
+    public static void fillWithWeights(WeightedGraph<String> graph) {
+        graph.addEdge("Almaty", "Astana", 4.5);
+        graph.addEdge("Shymkent", "Atyrau", 17.8);
+        graph.addEdge("Atyrau", "Astana", 15.4);
+        graph.addEdge("Almaty", "Shymkent", 3.2);
+        graph.addEdge("Shymkent", "Astana", 7.7);
+        graph.addEdge("Astana", "Kostanay", 2.3);
+        graph.addEdge("Shymkent", "Kyzylorda", 2.0);
+    }
+
+
+
     public static void outputPath(Search<String> search, String key) {
         for (String v : search.pathTo(key)) {
             System.out.print(v + " -> ");
         }
         System.out.println();
     }
+
+
 }
